790c9d83755240d194d2c232bd7eb5177ea9060d
Jesus-A-S-Git <168792729+Jesus-A-S-Git@users.noreply.github.com>
2024-06-28 16:10:23 -0700

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,7 @@ document.getElementById('contactForm').addEventListener('submit', function(event
     event.preventDefault();
     alert('Form submitted successfully!');
 });
+// Setting a custom cursor for the entire website
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.style.cursor = 'url(../images/cursor.png), auto';
+});
